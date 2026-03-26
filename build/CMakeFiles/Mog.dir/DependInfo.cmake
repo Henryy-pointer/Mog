c0e7dc47e16da149af55bd8ff00b6b5ff1d568e6
henryy <henryy.r3@protonmail.com>
2026-03-26 19:58:00 -0300

    build directory?

---
+++ build/CMakeFiles/Mog.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Proyectos/C/Mog/src/Scene.c" "CMakeFiles/Mog.dir/src/Scene.c.o" "gcc" "CMakeFiles/Mog.dir/src/Scene.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/app/cube.c" "CMakeFiles/Mog.dir/src/app/cube.c.o" "gcc" "CMakeFiles/Mog.dir/src/app/cube.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/app/image.c" "CMakeFiles/Mog.dir/src/app/image.c.o" "gcc" "CMakeFiles/Mog.dir/src/app/image.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/main.c" "CMakeFiles/Mog.dir/src/main.c.o" "gcc" "CMakeFiles/Mog.dir/src/main.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/platform/X11/window.c" "CMakeFiles/Mog.dir/src/platform/X11/window.c.o" "gcc" "CMakeFiles/Mog.dir/src/platform/X11/window.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/camera.c" "CMakeFiles/Mog.dir/src/renderer/camera.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/camera.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/framebuffer-functions.c" "CMakeFiles/Mog.dir/src/renderer/framebuffer-functions.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/framebuffer-functions.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/matrix-functions.c" "CMakeFiles/Mog.dir/src/renderer/matrix-functions.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/matrix-functions.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/model-transform.c" "CMakeFiles/Mog.dir/src/renderer/model-transform.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/model-transform.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/model.c" "CMakeFiles/Mog.dir/src/renderer/model.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/model.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/projection.c" "CMakeFiles/Mog.dir/src/renderer/projection.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/projection.c.o.d"
+  "/mnt/d/Proyectos/C/Mog/src/renderer/rasterizer.c" "CMakeFiles/Mog.dir/src/renderer/rasterizer.c.o" "gcc" "CMakeFiles/Mog.dir/src/renderer/rasterizer.c.o.d"
+  "" "Mog" "gcc" "CMakeFiles/Mog.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
